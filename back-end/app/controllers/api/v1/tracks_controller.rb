4748350e62c318127dd6c38d248c5ed328364c0c
harim1206 <harim1206@gmail.com>
2018-06-07 10:37:10 -0400

    Added show route in Tracks controller

--- back-end/app/controllers/api/v1/tracks_controller.rb
+++ back-end/app/controllers/api/v1/tracks_controller.rb
@@ -1,11 +1,23 @@
-module Api
-  module V1
-    class TracksController < ApplicationController
+class Api::V1::TracksController < ApplicationController
+  before_action :set_track, only: [:show]
 
-      def index
-        render json: Cat.all
-      end
-
-    end
+  def index
+    render json: Track.all
   end
+
+  def show
+    render json: @track
+  end
+
+  private
+
+  # def todo_params
+  #   # whitelist params
+  #   params.permit(:title, :created_by)
+  # end
+
+  def set_track
+    @track = Track.find(params[:id])
+  end
+
 end
