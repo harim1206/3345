d6b6694323c5ba43b095f73aefb4bf913558299d
harim1206 <harim1206@gmail.com>
2018-06-07 17:01:18 -0400

    Basic Discogs API testing in seed file

--- back-end/app/models/track.rb
+++ back-end/app/models/track.rb
@@ -1,2 +1,7 @@
 class Track < ApplicationRecord
+
+
+
+
+
 end
--- back-end/db/seeds.rb
+++ back-end/db/seeds.rb
@@ -5,8 +5,63 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "discogs-wrapper"
+require "byebug"
+require "json"
+require "awesome_print"
+
+puts ("hello")
+wrapper = Discogs::Wrapper.new("api")
+collection = wrapper.get_user_collection("harim1206")
+
+byebug
+ap ("collection: #{collection}")
 
 
-Track.create(title: "Hello World", artist: "MJ", release: "First")
-Track.create(title: "Cup", artist: "Khruangbin", release: "Second")
-Track.create(title: "Fun", artist: "Toto", release: "Third")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# artist = wrapper.get_artist("329937")
+
+# You must be authenticated in order to search. I provide a few ways to do this. See the AUTHENTICATION section below.
+# auth_wrapper = Discogs::Wrapper.new("api", user_token: "vujWxYfgzKJSAfdwkAOZmZdmyaHxWUiBccyQOhst")
+# search       = auth_wrapper.search("Moodymann", :per_page => 10, :type => :artist)
+#
+# ap search
+#
+# byebug
+#
+# puts(JSON.parse(search))
+#
+# puts("search #{search.pagination.items}")           # => 2
+# puts("search #{search.results.first.title}")           # => "Necrovore"
+# puts("search #{search.results.first.type}")           # => "artist"
+# puts("search #{search.results.first.id}")           # => 691078
+#
+# puts("artist.name: #{artist.name}")
+# puts(artist.members.count)                 # => 4
+# puts(artist.members.first.name)            # => "Mark Shelton"
+# puts(artist.profile)                       # => "Heavy Metal band from ..."
+
+
+#
+#
+# Track.create(title: "Hello World", artist: "MJ", release: "First")
+# Track.create(title: "Cup", artist: "Khruangbin", release: "Second")
+# Track.create(title: "Fun", artist: "Toto", release: "Third")
